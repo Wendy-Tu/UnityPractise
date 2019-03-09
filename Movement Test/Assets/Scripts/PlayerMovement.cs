43555c1b9c32939449b9099cbd2c53c669fe71b5
Ozair Zaman <ozair.zaman@outlook.com>
2019-03-09 22:46:08 +1100

    movement study
    movement with CharacterController.Move()
    movement with transform.Translate()

---
+++ Movement Test/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float movementSpeed;
+    public float myCelsius;
+    public float myFarenheit;
+
+
+
+    // Use this for initialization
+    void Start () {
+        // movementSpeed = 1f;
+        myCelsius = 1f;
+
+        myFarenheit = myCelsius * 9 / 5 + 32;
+
+        print(myFarenheit);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.Translate(movementSpeed*Input.GetAxis("Horizontal")*Time.deltaTime, 0, movementSpeed*Input.GetAxis("Vertical") * Time.deltaTime);
+	}
+}
---
+++ Practise/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Wendy's Codes/Movement/Player")]
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController charController;
+    public Vector3 directionInfo;
+    public float force = 5;
+    public float gravity = 20;
+    public float jumpForce = 20;
+	// Use this for initialization
+	void Start () {
+        charController = this.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        if (charController.isGrounded)
+        {
+            directionInfo = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            // direction * a force = movment
+            //directionInfo *= force;
+            directionInfo = directionInfo * force;
+
+
+
+            if (Input.GetButton("Jump"))
+            {
+                directionInfo.y = jumpForce;
+            }
+        }
+ 
+
+        directionInfo.y -= gravity * Time.deltaTime; //this emulates gravity
+        //now lets move the object using our character controller
+        charController.Move(directionInfo * Time.deltaTime);
+        
+	}
+}
